66243a289b133e8bd831157dde53280090cb729f
Owen Dostie <owendostie@gmail.com>
2024-08-26 20:32:38 -0400

    final commit

---
+++ server/database.ts
@@ -0,0 +1,13 @@
+// required methods
+
+// init (if some/all tables are missing)
+
+// active_sessions
+// server-side information abou t all currently connected clients 
+    // session id, username, in lobby, date connected, last action & timestamp, 
+
+// song files
+    // song id, round id, game, username
+
+// games 
+// a history of all the games that have been played and the users
